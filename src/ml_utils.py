06f52ca8601e4adbe6e448a21b99bd704ffcef38
elissamim <mouad.el-issami@igf.finances.gouv.fr>
2025-04-17 14:00:24 +0000

    (refacto) linting

--- src/ml_utils.py
+++ src/ml_utils.py
@@ -18,8 +18,7 @@ nlp = spacy.load("fr_core_news_sm")
 stop_words = set(stopwords.words("french"))
 
 
-def nltk_text_preprocessing(x: str, 
-                           lemmatization:bool=False) -> str:
+def nltk_text_preprocessing(x: str, lemmatization: bool = False) -> str:
     """
     Preprocess textual data using tokenization, pucntuation removal and lowering cases.
 
@@ -35,34 +34,30 @@ def nltk_text_preprocessing(x: str,
     x = x.lower()
 
     # Word tokens for French language
-    tokens = word_tokenize(x, 
-                           language = "french")
+    tokens = word_tokenize(x, language="french")
 
     # Stopwords and punctuation removal
     tokens = [
-        token for token in tokens
+        token
+        for token in tokens
         if token not in stop_words and token not in string.punctuation
     ]
 
     # French stemming
     if not lemmatization:
         stemmer = FrenchStemmer()
-        
-        stemmed_tokens = [
-            stemmer.stem(token) for token in tokens
-        ]
-        
+
+        stemmed_tokens = [stemmer.stem(token) for token in tokens]
+
         return " ".join(stemmed_tokens)
 
     # French lemmatization
     doc = nlp(" ".join(tokens))
-    lemmatized_tokens = [
-        token.lemma_ for token in doc
-    ]
+    lemmatized_tokens = [token.lemma_ for token in doc]
     return " ".join(lemmatized_tokens)
 
-class MeanEmbeddingVectorizer(BaseEstimator, TransformerMixin):
 
+class MeanEmbeddingVectorizer(BaseEstimator, TransformerMixin):
     def __init__(self, model):
         self.model = model
         self.vector_size = model.vector_size
@@ -71,12 +66,17 @@ class MeanEmbeddingVectorizer(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X):
-        return np.array([
-            np.mean(
-                [self.model.wv[word] for word in doc.split() if word in self.model.wv]
-                or [np.zeros(self.vector_size)],
-                axis=0
-            )
-            for doc in X
-        ])
-    
+        return np.array(
+            [
+                np.mean(
+                    [
+                        self.model.wv[word]
+                        for word in doc.split()
+                        if word in self.model.wv
+                    ]
+                    or [np.zeros(self.vector_size)],
+                    axis=0,
+                )
+                for doc in X
+            ]
+        )
