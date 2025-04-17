61ec605ae8ee9865e98e78da953a8737078518b7
elissamim <mouad.el-issami@igf.finances.gouv.fr>
2025-04-17 10:28:30 +0000

    (add) lemmatization in French

--- src/ml_utils.py
+++ src/ml_utils.py
@@ -4,19 +4,26 @@ from nltk.tokenize import word_tokenize
 from nltk.stem.snowball import FrenchStemmer
 import string
 import spacy
+import spacy.cli
 
 nltk.download("punkt")
 nltk.download("punkt_tab")
 nltk.download("stopwords")
 
+spacy.cli.download("fr_core_news_sm")
+nlp = spacy.load("fr_core_news_sm")
+
 stop_words = set(stopwords.words("french"))
 
-def nltk_text_preprocessing(x: str) -> str:
+
+def nltk_text_preprocessing(x: str, 
+                           lemmatization:bool=False) -> str:
     """
     Preprocess textual data using tokenization, pucntuation removal and lowering cases.
 
     Args:
         x (str): Text to preprocess.
+        lemmatization (bool, Optional): Apply lemmatization (True) or not (False). Defaults to False.
 
     Returns:
         str: Preprocessed text.
@@ -36,11 +43,19 @@ def nltk_text_preprocessing(x: str) -> str:
     ]
 
     # French stemming
-    stemmer = FrenchStemmer()
-    stemmed_tokens = [
-        stemmer.stem(token) for token in tokens
-    ]
+    if not lemmatization:
+        stemmer = FrenchStemmer()
+        
+        stemmed_tokens = [
+            stemmer.stem(token) for token in tokens
+        ]
+        
+        return " ".join(stemmed_tokens)
 
     # French lemmatization
-
-    return " ".join(stemmed_tokens)
+    doc = nlp(" ".join(tokens))
+    lemmatized_tokens = [
+        token.lemma_ for token in doc
+    ]
+    return " ".join(lemmatized_tokens)
+    
