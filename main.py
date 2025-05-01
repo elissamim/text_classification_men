7c44084dae382ddc7161fa01a7f781680e79e948
elissamim <mouad.el-issami@igf.finances.gouv.fr>
2025-05-01 17:28:55 +0000

    (add) tuned camembert model

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from transformers import (
+                         CamembertTokenizer,
+                         CamembertForSequenceClassification
+                         )
 import joblib
 import sys
+import torch
 
 sys.path.append("src")
 from ml_utils import *
 from few_shot_classification import *
 
+# Load ML model
 ml_model = joblib.load("models/ml_model.joblib")
+
+# Load Camembert Model
+camembert_model = CamembertForSequenceClassification.from_pretrained("models/camembert_model")
+camembert_tokenizer = CamembertTokenizer.from_pretrained("models/camembert_model")
+camembert_model.eval()
 
 app = FastAPI(title="Multi-Model Text Classification API")
 
@@ -25,13 +36,20 @@ def predict(request: TextRequest):
     if model_name == "ml":
         
         clean_text = nltk_text_preprocessing(text)
-        return {"prediction":ml_model.predict([clean_text])[0]}
+        return {"prediction_ml":ml_model.predict([clean_text])[0]}
         
     elif model_name == "camembert":
-        pass
+
+        inputs = camembert_tokenizer(text, return_tensors="pt", truncation=True, padding=True, max_length=512)
+        with torch.no_grad():
+            outputs = camembert_model(**inputs)
+            probs = torch.softmax(outputs.logits, dim=1)
+            predicted_label = torch.argmax(probs, dim=1).item()
+
+        return {"prediction_camembert":int(predicted_label)}
         
     elif model_name == "fewshot":
-        return {"prediction":few_shot_classification(text)}
+        return {"prediction_fewshot":few_shot_classification(text)}
         
     else:
         raise HTTPException(status_code=400,
